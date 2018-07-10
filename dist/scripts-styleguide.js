179cb46fecf1465aa8781dd428bdba585d650f72
M Miller <miller.2676@osu.edu>
2018-07-10 15:51:48 -0700

    auto-generated commit via pl-deploy

--- dist/scripts-styleguide.js
+++ dist/scripts-styleguide.js
@@ -177,6 +177,25 @@ Drupal.behaviors.tabs = {
     }
   }
 };
+'use strict';
+
+(function ($) {
+
+  Drupal.behaviors.blockquote = {
+    attach: function attach(context, settings) {
+
+      $(context).find('blockquote').each(function () {
+
+        var pq_icon = '<div class="pulled-quote__icon"><i class="fas fa-quote-left fa-3x"></i></div>';
+
+        if ($(this).hasClass('pulled-quote')) {
+          $('.pulled-quote p').wrap('<div class="pulled-quote__text"></div>');
+          $('.pulled-quote__text').before(pq_icon);
+        }
+      });
+    }
+  };
+})(jQuery);
 /**
  * @file
  * A JavaScript file containing the main menu functionality (small/large screen)
@@ -226,23 +245,4 @@ Drupal.behaviors.tabs = {
 // })(Drupal); // UNCOMMENT IF DRUPAL.
 */
 "use strict";
-'use strict';
-
-(function ($) {
-
-  Drupal.behaviors.blockquote = {
-    attach: function attach(context, settings) {
-
-      $(context).find('blockquote').each(function () {
-
-        var pq_icon = '<div class="pulled-quote__icon"><i class="fas fa-quote-left fa-3x"></i></div>';
-
-        if ($(this).hasClass('pulled-quote')) {
-          $('.pulled-quote p').wrap('<div class="pulled-quote__text"></div>');
-          $('.pulled-quote__text').before(pq_icon);
-        }
-      });
-    }
-  };
-})(jQuery);
 //# sourceMappingURL=scripts-styleguide.js.map
